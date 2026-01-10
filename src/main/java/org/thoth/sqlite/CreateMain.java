0bfdc6f73a0dfaac73d9b82e5d91dbb6cd6b05e0
Michael Remijan <mjremijan@yahoo.com>
2026-01-09 19:39:37 -0600

    SQLite research

--- src/main/java/org/thoth/sqlite/CreateMain.java
+++ src/main/java/org/thoth/sqlite/CreateMain.java
@@ -3,7 +3,6 @@ package org.thoth.sqlite;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  *
@@ -12,41 +11,27 @@ import java.sql.ResultSet;
 public class CreateMain {
 
     public static void main(String[] args) throws Exception {
-        //String url = "jdbc:sqlite:example.db";
-        String url = "jdbc:sqlite:D:/Documents/Databases/SQLite/anw.db";
+        String sql;
+        String url 
+//            = "jdbc:sqlite:example.db";
+            = "jdbc:sqlite:D:/Documents/Databases/SQLite/example.db";
         Connection conn = DriverManager.getConnection(url);
         if (conn != null) {
             System.out.println("Connected to SQLite database.");
         }
         
-        String sql = """
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                email TEXT UNIQUE
+        sql = """
+            create table if not exists user (
+                  id integer primary key autoincrement
+                , name text unique not null
             );
             """;     
-        
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.execute();
-            System.out.println("Table created.");
+            System.out.println("The gyms table created.");
         } catch (Exception e) {
             throw e;
-        }
-        
-        
-        sql = """
-            select * from users
-            """;     
-        
-        try (PreparedStatement stmt = conn.prepareStatement(sql);
-                ResultSet rs = stmt.executeQuery()) {
-            while (rs.next()) {
-                System.out.printf("%s, %s, %s%n", rs.getString("id"), rs.getString("name"), rs.getString("email"));
-            }
-            System.out.println("Table created.");
-        } catch (Exception e) {
-            throw e;
-        }
+        }     
+        System.out.printf("Done!%n");
     }
 }
